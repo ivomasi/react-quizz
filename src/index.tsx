a3f777a8b47819dd49fa321d83ad6316112aa0f9
ivomasi <ivo.masiar7@gmail.com>
2021-05-03 09:03:00 +0200

    global styles moved to to level app.styles from quiz.styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import Quiz from "./Quiz/Quiz";
 
 import MainMenu from "./MainMenu/MainMenu";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainMenu />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
